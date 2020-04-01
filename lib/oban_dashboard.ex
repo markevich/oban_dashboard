3c645631d391f333dd6405bb1a9ade37e41afea4
markevich <slava.markevich@gmail.com>
2020-04-01 12:39:24 +0000

    Generate mix project

---
+++ lib/oban_dashboard.ex
@@ -0,0 +1,18 @@
+defmodule ObanDashboard do
+  @moduledoc """
+  Documentation for `ObanDashboard`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ObanDashboard.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oban_dashboard_test.exs
@@ -0,0 +1,8 @@
+defmodule ObanDashboardTest do
+  use ExUnit.Case
+  doctest ObanDashboard
+
+  test "greets the world" do
+    assert ObanDashboard.hello() == :world
+  end
+end
