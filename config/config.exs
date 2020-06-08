24bd47c4decc46ff35578a0edfb39fdeb1b55151
Slava Markevich <slava.markevich@gmail.com>
2020-06-08 10:37:49 +0300

    Configure dev database. Add oban dependency (#2)

--- config/config.exs
+++ config/config.exs
@@ -6,3 +6,16 @@ config :logger, level: :warn
 config :logger, :console, format: "[$level] $message\n"
 
 config :phoenix, :stacktrace_depth, 20
+
+# temporary
+config :oban_dashboard, ecto_repos: [ObanDashboard.Test.Repo]
+
+config :oban_dashboard, ObanDashboard.Test.Repo,
+  priv: "test/support/",
+  database: "oban_dashboard",
+  username: "postgres",
+  password: "",
+  hostname: "localhost",
+  port: "5432"
+
+#
